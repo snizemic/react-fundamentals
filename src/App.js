4cc4301c1b52bbe7d395f96419e51b4750440ff8
Michal Snizek <snizemic@gmail.com>
2017-01-02 23:15:03 +0100

    dalsi kodovani

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Link, IndexLink } from 'react-router';
+//import logo from './logo.svg';
 import './App.css';
-import Test from './Test';
+import Home from './Home';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div>
+        <h1>React Router Tutorial</h1>
+        <ul role="nav">
+          <li><IndexLink to="/" activeClassName="active">Home</IndexLink></li>
+          <li><Link to="/about" activeClassName="active">About</Link></li>
+          <li><Link to="/repos/karel/programy" activeClassName="active">Karel</Link></li>
+          <li><Link to="/repos/jirka/databaze" activeClassName="active">Jirka</Link></li>
+        </ul>
+        <div>
+          {this.props.children || Home}
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload. <Test />
-        </p>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, IndexRoute, hashHistory } from 'react-router';
-import Test from './Test';
+import App from './App';
 import Home from './Home';
 import About from './About';
 import Repos from './Repos';
@@ -9,7 +9,7 @@ import Repos from './Repos';
 
 ReactDOM.render((
   <Router history={hashHistory}>
-    <Route path="/" component={Test}>
+    <Route path="/" component={App}>
       <IndexRoute component={Home} />
       <Route path="/about" component={About} />
       <Route path="/repos/:userName/:repoName" component={Repos} />
